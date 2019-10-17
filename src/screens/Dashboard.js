3bb895edb1f573b73b62da816d9427b87a3b43da
Dan Hromada <hromada.dan@gmail.com>
2019-10-17 23:02:02 +0200

    Dashboard grid

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Headline from '../components/Headline';
 
 export default function DashboardScreen({ location, id }) {
     return (
         <div className="dashboard-screen">
-            <p style={{ fontSize: '48px', color: 'lightgray' }}>¯\_(ツ)_/¯</p>
+            <div className="dashboard-screen__tokens">
+                <section>
+                    <Headline>Long positions</Headline>
+                </section>
+                <section>
+                    <Headline>Short positions</Headline>
+                </section>
+                <section>
+                    <Headline>Your bundles</Headline>
+                </section>
+            </div>
+
+            <div className="dashboard-screen__headline">
+                <Headline>CREATE YOUR BUNDLE</Headline>
+            </div>
+            <div className="dashboard-screen__configurator"></div>
+            <div className="dashboard-screen__bundle-templates">
+                <section>
+                    <Headline>BUNDLE TEMPLATES</Headline>
+                </section>
+            </div>
+            <div className="dashboard-screen__current-prices">
+                <section>
+                    <Headline>CURRENT PRICES</Headline>
+                </section>
+            </div>
         </div>
     );
 }
