6f5542e4dee54efda3ab3fea149e6e14c41487c9
Dan Hromada <hromada.dan@gmail.com>
2019-10-21 05:48:47 +0200

    Fix tests

--- src/Application.test.js
+++ src/Application.test.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './Application';
 
+const initialState = {
+    wallet: null,
+    tokens: [],
+    bundles: [],
+    templates: [],
+    pricesCurrency: 'USD',
+    prices: {
+        ETH: 0,
+        DAI: 0,
+    },
+    configuratorTokens: {
+        long: null,
+        short: null,
+    },
+};
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Application />, div);
+    ReactDOM.render(
+        <Application dispatch={() => {}} state={initialState} />,
+        div,
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
